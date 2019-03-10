ccade6c4e18d9cbe78673a343829a5e3f5b7dc8a
nicoga97 <n19972016>
2019-03-10 10:08:22 -0500

    Project creation

---
+++ src/main/java/eci/ieti/taskplannerAPI/TaskPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package eci.ieti.taskplannerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/ieti/taskplannerAPI/TaskPlannerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package eci.ieti.taskplannerAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskPlannerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
